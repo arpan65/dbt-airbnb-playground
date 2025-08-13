ff022c6b0985a298d17933d05aa4ac4fd55bc90a
Arpan Das <arpan@Arpans-MacBook-Air.local>
2025-08-13 13:39:30 -0400

    final cleanup

--- my_snowflake_project/macros/test_dim_listings_min_nights.sql
+++
@@ -1,9 +0,0 @@
-dim_listings-- macros/test_dim_listings_min_nights.sql
-
-{% test dim_listings_min_nights(model) %}
-
-SELECT *
-FROM {{ model }}
-WHERE minimum_nights < 1
-
-{% endtest %}
--- my_snowflake_project/models/dim/dim_hosts_cleansed.sql
+++
@@ -1,7 +0,0 @@
-WITH dim_hosts_cleansed AS (
-    SELECT
-    host_id,
-    NVL(host_name, 'Anonymous') AS host_name,created_at,updated_at
-    FROM {{ref('src_hosts')}}
-)
-SELECT * FROM dim_hosts_cleansed
